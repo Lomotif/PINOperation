c96ea4593ebf91aa6fbaca0d437444e10d4223f0
Casey Law <sacred0214@gmail.com>
2020-04-22 12:11:21 +0800

    Added support for Swift Package Manager

--- Source/PINOperation.h
+++ Source/PINOperation.h
@@ -6,7 +6,14 @@
 //  Copyright © 2017 Pinterest. All rights reserved.
 //
 
+#ifdef SWIFT_PACKAGE
+#import <PINOperationMacros.h>
+#import <PINOperationTypes.h>
+#import <PINOperationQueue.h>
+#import <PINOperationGroup.h>
+#else
 #import <PINOperation/PINOperationMacros.h>
 #import <PINOperation/PINOperationTypes.h>
 #import <PINOperation/PINOperationQueue.h>
 #import <PINOperation/PINOperationGroup.h>
+#endif
